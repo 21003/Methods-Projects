5200f44d633d5ff8f374f2c03d08d5e81948f31e
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-03-14 14:14:33 -0400

    third method is done

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,29 +6,39 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard;
-        keyboard = new Scanner (System.in);
+        keyboard = new Scanner(System.in);
+
 
         String userName;
+        int firstNumber, secondNumber, answer1;
 
-        System.out.println("What is your name ? ");
+
+        System.out.println("What is your name");
         userName = keyboard.nextLine();
+        System.out.println("Can you please give me one number");
+        firstNumber = keyboard.nextInt();
+        System.out.println("can you please give me one number");
+        secondNumber = keyboard.nextInt();
+        System.out.println(firstNumber + " times " + secondNumber + " equals " + mulitiply(firstNumber, secondNumber));
 
         printGreeting("World");
 
-        helloName( "" + userName);
+        helloName("" + userName);
+
+        System.out.println(firstNumber * secondNumber);
 
     }
 
-    public static void printGreeting(String noun){
-        System.out.println("Hello "+noun);
 
+    public static void printGreeting(String noun) {
+        System.out.println("Hello " + noun);
     }
+
     public static void helloName(String userName) {
-        System.out.println("Hello "+userName );
-
-    }
-    
-
-
+        System.out.println("Hello " + userName);
     }
 
+    public static int mulitiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
